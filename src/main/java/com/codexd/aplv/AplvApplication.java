b89379d70257402355d0eb09b20a05b6caf6e8a3
CristhiamPoma <cristhiam.p@ncodingbol.com>
2023-05-09 16:02:10 -0400

    create project

---
+++ src/main/java/com/codexd/aplv/AplvApplication.java
@@ -0,0 +1,13 @@
+package com.codexd.aplv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codexd/aplv/AplvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codexd.aplv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
